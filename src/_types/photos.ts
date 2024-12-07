dc55f61fb92a01ea550ac3b91cdea00f3e8401b6
dian afrial <agil.rahadi@gmail.com>
2024-12-08 00:15:47 +0700

    perf: photos api service include its types

--- src/_types/photos.ts
+++ src/_types/photos.ts
@@ -1,15 +1,58 @@
-export type SearchPhotosParamProps = {
+export enum EOrderByProps {
+  relevant = "Relevance",
+  latest = "Latest",
+  oldest = "Oldest"
+}
+
+export enum EContentFilterProps {
+  low = "Low",
+  high = "High",
+}
+
+export enum EColorProps {
+  black_and_white = "Black & White",
+  black = "Black",
+  white = "White",
+  yellow = "Yellow",
+  red = "Red",
+  blue = "Blue",
+  magenta = "Magenta",
+  green = "Green",
+  teal = "Teal",
+  orange = "Orange"
+}
+
+export enum EOrientationProps {
+  landscape = "Landscape",
+  portrait = "Portrait",
+  squarish = "Squarish"
+}
+
+
+export type OrderByObject = Record<keyof typeof EOrderByProps, string> 
+// const d = Object.entries(OrderByProps).map((e,v)=> console.log(e))
+
+export type SearchPhotosBaseParams = {
   query:string
   page:number 
   per_page:number
-  order_by: 'relevant' | 'latest'
-  content_filter: 'low' | 'high'
-  color?: 'black_and_white' | 'black' | 'white' | 'yellow'
-  orientation: ''
+  order_by: keyof typeof EOrderByProps
+  content_filter: keyof typeof EContentFilterProps
+  color?: keyof typeof EColorProps
+  orientation: keyof typeof EOrientationProps
 } 
 
-// export const photosKeys = {
-//     all: ['photos'] as const,
-//     search: (state: State, sorting: Sorting) =>
-//       [...todoKeys.lists(), state, sorting] as const,
-//   }
+export type SearchPhotosParams = Partial<SearchPhotosBaseParams>
+
+export const photosKeys = {
+  all: ['photos'] as const,
+  search: (
+    query?:string, 
+    page?:number, 
+    per_page?:number, 
+    order_by?:keyof typeof EOrderByProps,
+    content_filter?: keyof typeof EContentFilterProps,
+    color?: keyof typeof EColorProps,
+    orientation?: keyof typeof EOrientationProps
+  ) => [...photosKeys.all, query, page, per_page, order_by, content_filter, color, orientation] as const,
+}
--- src/apis/photos.ts
+++ src/apis/photos.ts
@@ -3,24 +3,25 @@ import {
 } from "@tanstack/react-query"
 import { ApiHeader } from "@/lib/apiHeader"
 import fakeData from "@/data/search_query.json"
+import { photosKeys } from "@/_types/photos"
 
 
-type QueryProps = {
-    queryKey: QueryKey
-}
+export type QueryPhotosProps = QueryFunctionContext<ReturnType<typeof photosKeys["search"]>>
 
-export const searchQueryPhotos = async ({ queryKey }: QueryProps) => {
-    const [_key, exts ] = queryKey
-    const query = exts as any
+export const searchQueryPhotos = async ({ queryKey }:QueryPhotosProps) => {
+    const [_key, query] = queryKey
+    //onsole.log("query",  query)
+
+    const response = await fetch('https://a6538fe10a824d75a98a5889e00d9288.api.mockbin.io/')
     // const response = await fetch(
-    //     process.env.UNSPLASH_BASE_API + `search/photos?query=${query.q}`, {
+    //     process.env.UNSPLASH_BASE_API + `search/photos?query=${query}`, {
     //         headers:ApiHeader()
     //     })
 
-    // if (!response.ok) {
-    //     throw new Error('Network response was not ok')
-    // }
+    if (!response.ok) {
+        throw new Error('Network response was not ok')
+    }
 
     return fakeData
-   // return response.json()
+    // return response.json()
 }
