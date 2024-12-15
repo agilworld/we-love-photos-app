99593b7c5053f6ccc9240ff324d34c046366bc47
dian afrial <agil.rahadi@gmail.com>
2024-12-15 13:49:20 +0700

    fix: replace with Image component

--- src/app/components/PhotoGridItem.tsx
+++ src/app/components/PhotoGridItem.tsx
@@ -2,6 +2,7 @@
 
 import { PhotoResult } from "@/_types/photos"
 import { useEffect, useState } from "react";
+import Image, { ImageLoader, ImageLoaderProps } from "next/image"; 
 import type { ComponentPropsWithRef } from "react";
 import {
     Drawer,
@@ -11,22 +12,26 @@ import {
     DrawerFooter,
     DrawerHeader,
     DrawerTitle,
-    DrawerTrigger,
   } from "@/components/ui/drawer"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
+import imagedefault from "../images/love-placeholder-small.jpg"
 
 type PhotoGridItemProps = {
     item:Partial<PhotoResult>
+    isFetching: boolean
+    isLast: boolean
 } & ComponentPropsWithRef<"div">
 
-export default function PhotoGridItem({item, ...rest} : PhotoGridItemProps) {
+export default function PhotoGridItem({item, isFetching, isLast, ...rest} : PhotoGridItemProps) {
     return (
         <div key={item.id} className="cursor-pointer" {...rest} >
             <img 
                 className="h-auto max-w-full rounded-lg shadow-lg hover:scale-105 duration-200 delay-75	ease-in-out" 
-                src={item.urls?.small} />  
-                     
+                src={item.urls?.small} />
+            {isLast && isFetching && <Skeleton 
+                className="w-full mt-10 h-[250px] rounded-lg" /> }
         </div>
     )
 }
@@ -36,53 +41,97 @@ type PhotoDrawerProps = {
     item:PhotoResult 
     onCloseDrawer:()=>void
 }
+
+const imageLoader = ({ src, width, quality }:ImageLoaderProps) => {
+    return src
+}
+
 export const PhotoDetailDrawer = ({item, onCloseDrawer}:PhotoDrawerProps) => {
     const [open, setOpen] = useState<boolean>(true)
+    const [isClient, setIsClient] = useState(false)
+ 
+    useEffect(() => {
+      setIsClient(true)
+    }, [])
+
     const handleClose = () => {
-        console.log("handleClose", false)
-        onCloseDrawer()
         setOpen(false)
+
+        setTimeout(()=>{
+            onCloseDrawer()
+        }, 500)  
     }
 
     const downloadPhoto = () => {
         window.open(item.urls.full, "_blank")
     }
 
+    const initialName = () => {
+        let f, l
+        if( item.user.first_name && item.user.last_name ) {
+            f = item.user.first_name.charAt(0)
+            l = item.user.last_name.charAt(0)
+        } else {
+            f = item.user.name.charAt(0)
+            l = item.user.name.charAt(1)
+        }
+        return `${f}${l}`
+    }
+
+    const desc = () => {
+       return item.description?.slice(0, 100) ?? '' + 
+        (item.description?.length >= 100 ? '...' : '')
+    }
+
+    const altDesc = () => {
+        return item.alt_description?.slice(0, 100) ?? '' + 
+         (item.alt_description?.length >= 100 ? '...' : '')
+     }
+
     return (
         <Drawer open={open} onClose={handleClose} onOpenChange={handleClose}>
             <DrawerContent>
                 <div className="mx-auto w-full max-w-4xl py-6">
-                    <div className="flex">
+                    <div className="flex justify-center">
                         <div className="w-4/6">
-                            <img src={item.urls.regular} className="rounded-lg" />
+                            {item.urls?.regular && isClient && <Image 
+                                src={item.urls?.regular} 
+                                alt={item.alt_description}
+                                loader={imageLoader}
+                                style={{ 
+                                    objectFit: "contain", 
+                                    resize:"horizontal"
+                                }}	
+                                width={item.width/5}
+                                height={item.height/5}
+                                loading="lazy"
+                                className={`max-h-[80vh]`} />}
                         </div>
                         <div className="w-2/6">
                             <DrawerHeader className="text-left pt-0 pl-10">
                                 <DrawerTitle className="leading-tight">
-                                    {item.description}</DrawerTitle>
+                                    {desc()}</DrawerTitle>
                                 <DrawerDescription className="leading-none text-md">
-                                    {item.alt_description}                  
+                                    {altDesc()}                  
                                 </DrawerDescription>
                                 <div className="flex items-center my-6">
                                     <Avatar className="mr-3">
                                         <AvatarImage 
                                             src={item.user.profile_image.medium} />
-                                        <AvatarFallback>{item.user.first_name.charAt(0)}{item.user.last_name.charAt(0)}</AvatarFallback>
+                                        <AvatarFallback>{initialName()}</AvatarFallback>
                                     </Avatar>
                                     <div className="text-xs">
                                         <h4 className="font-regular">
                                             {item.user.name} </h4>
                                         <div>
-                                            <a className="text-blue-600 visited:text-purple-600" href={item.user.portfolio_url} target="_blank">View Profile</a>
+                                            {item.user.portfolio_url && <a className="text-blue-600 visited:text-purple-600" href={item.user.portfolio_url} target="_blank">View Profile</a>}
                                         </div>
                                     </div>
                                 </div>
                             </DrawerHeader>
-                            <DrawerFooter className="pt-2">
+                            <DrawerFooter className="mt-10 pt-2">
                                 <Button onClick={downloadPhoto}>View</Button>
-                                <DrawerClose asChild>
-                                    <Button variant="outline">Cancel</Button>
-                                </DrawerClose>
+                                <Button onClick={handleClose} variant="outline">Cancel</Button>
                             </DrawerFooter>
                         </div>
                     </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { PhotoResult } from "@/_types/photos"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -17,3 +18,52 @@ export function chunks2Arr(arr:any[], size:number) {
 }
 
 export const uniqueBy = (x:any,f:any)=>Object.values(x.reduce((a:any,b:any)=>((a[f(b)]=b),a),{}));
+
+export const chunk3dAdvanceByHeight = (arr:PhotoResult[]) => {
+  let temp:any = []
+  let totalH = 0
+  let newArr:typeof arr[] = []
+  arr.map((item)=>{
+    let heightScaleRatio = 0
+    if(item.width < item.height ) {
+      heightScaleRatio = (item.height / item.width) * item.height
+    } else {
+      heightScaleRatio = item.height
+    } 
+    
+    totalH +=Math.floor(heightScaleRatio/10)
+    temp.push({id:item.id, value:totalH})
+  })
+
+  console.log("temp", temp)
+
+  let splitterIdx = [0]
+  let divideBy13cols = totalH * (1 / 3)
+  let divideBy23cols = totalH * (2 / 3)
+
+  temp.find((item:any, index:number)=>{
+    if(divideBy13cols > item.value && divideBy13cols < temp[index+1].value  ) {
+      let deviationBottom = Math.ceil(divideBy13cols) - item.value
+      let deviationUp = temp[index+1].value - Math.ceil(divideBy13cols)
+      if( deviationUp >= deviationBottom ) splitterIdx.push(index+1) 
+          else splitterIdx.push(index+1)
+      
+    }
+
+    if(divideBy23cols > item.value && divideBy23cols < temp[index+1].value  ) {
+      let deviationBottom = Math.ceil(divideBy23cols) - item.value
+      let deviationUp = temp[index+1].value - Math.ceil(divideBy23cols)
+      if( deviationUp >= deviationBottom ) splitterIdx.push(index+1) 
+          else splitterIdx.push(index+1)
+      
+    }
+  })
+  console.log("splitterIdx", splitterIdx)
+  splitterIdx.map((maxIndex, i)=>{
+    let startIndex = maxIndex > 0 ? maxIndex + 1 : 0
+    let endIndex = splitterIdx[i+1] ? splitterIdx[i+1] + 1 : undefined
+    newArr.push(arr.slice(startIndex, endIndex))
+  })
+ 
+  return newArr
+}
