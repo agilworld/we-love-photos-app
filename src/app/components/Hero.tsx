646ad5d59d286a611633d3ee925f2b84c0ded341
dian afrial <agil.rahadi@gmail.com>
2024-12-15 15:00:42 +0700

    fix: wording

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,8 +4,8 @@ export default function Hero(){
     return (
         <div className="flex flex-col items-center my-12 text-center">
              <Heart size={64} color="red"/>
-            <H2 className="mb-2">We love photos</H2>
-            <H4 className="font-normal text-neutral-500">Share what your love for free here</H4>
+            <H2 className="mb-2">We Love Photos</H2>
+            <H4 className="font-normal text-neutral-500">Find photos and share what your love for free!</H4>
         </div>
 
     )
