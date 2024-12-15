44dd34f4117ea893d9ee4b7e2e80ebc923f588e8
dian afrial <agil.rahadi@gmail.com>
2024-12-15 15:00:28 +0700

    feat: add header comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import PhotoGrid from "./components/PhotoGrid";
 import SearchBar from "./components/SearchBar";
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="container container-lg w-4/6 mx-auto px-4">
-        <div className="py-10">
-          <Hero />
-          <SearchBar />
-          <Footer />
-        </div>
+        <Header />
+        <Hero />
+        <SearchBar />
+        <Footer />
     </div>
   );
 }
