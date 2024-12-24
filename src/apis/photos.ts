5e35fae41110708531c3b6b86c99a1897a9cc979
dian afrial <agil.rahadi@gmail.com>
2024-12-24 19:54:55 +0700

    feat: add class encapsulation

--- src/apis/photos.ts
+++ src/apis/photos.ts
@@ -1,18 +1,42 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
-import { ApiHeader } from "@/lib/apiHeader";
-import { photosKeys, SearchPhotosProps } from "@/_types/photos";
-
+import { ApiHeader, PexelApiHeader } from "@/lib/apiHeader";
+import {
+  PexelSearchPhotosProps,
+  photosKeys,
+  UnsplashSearchPhotosProps,
+} from "@/_types/photos";
+import {
+  PhotoRepositoryList,
+  PhotoRepositoryPexel,
+} from "@/lib/photoRepository";
 export type QueryPhotosProps = QueryFunctionContext<
   ReturnType<(typeof photosKeys)["search"]>
 >;
 
 export const searchQueryPhotos: any = async ({
   queryKey,
-}: QueryPhotosProps): Promise<SearchPhotosProps> => {
+}: QueryPhotosProps): Promise<PhotoRepositoryList> => {
+  const [, query, page, per_page, color, orientation] = queryKey;
+
+  const [pexel] = await Promise.all([
+    //unsplashSearchPhotosApi(queryKey),
+    pexelSearchPhotosApi(queryKey),
+  ]);
+
+  const photoObjList = new PhotoRepositoryList();
+  photoObjList.setIteratorPhotoPexel(pexel);
+  //photoObjList.setIteratorPhotoUnsplash(unsplash);
+  return photoObjList;
+};
+
+export async function unsplashSearchPhotosApi(
+  queryKey: QueryPhotosProps["queryKey"],
+): Promise<UnsplashSearchPhotosProps> {
   const [, query, page, per_page, color, orientation] = queryKey;
   let urlApi =
     process.env.NEXT_PUBLIC_UNSPLASH_BASE_API +
     `search/photos?query=${query}&page=${page}&per_page=${per_page}`;
+
   if (color) {
     urlApi += `&color=${color}`;
   }
@@ -28,4 +52,29 @@ export const searchQueryPhotos: any = async ({
   }
 
   return response.json();
-};
+}
+
+export async function pexelSearchPhotosApi(
+  queryKey: QueryPhotosProps["queryKey"],
+): Promise<PexelSearchPhotosProps> {
+  const [, query, page, per_page, color, orientation] = queryKey;
+  let urlApi =
+    process.env.NEXT_PUBLIC_PEXEL_BASE_API +
+    `search?query=${query}&page=${page}&per_page=${per_page}`;
+
+  if (color) {
+    urlApi += `&color=${color}`;
+  }
+  if (orientation) {
+    urlApi += `&orientation=${orientation}`;
+  }
+  const response = await fetch(urlApi, {
+    headers: PexelApiHeader(),
+  });
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  return response.json();
+}
