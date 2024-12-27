0517fe9b20585b9b17f1c3b7ea7502e930020cbb
dian afrial <agil.rahadi@gmail.com>
2024-12-27 22:17:23 +0700

    fix: mode prod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
       >
         <Providers locale={locale} messages={messages}>
           {children}
-          <Analytics debug={true} />
+          <Analytics mode={"production"} />
         </Providers>
       </body>
     </html>
