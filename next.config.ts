d1c88e45afbc609629764b758dcc22c4e4757cda
dian afrial <agil.rahadi@gmail.com>
2024-12-29 21:27:27 +0700

    feat: add transformer model ai & config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,31 @@
 import type { NextConfig } from "next";
 import { withSentryConfig } from "@sentry/nextjs";
 import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  output: "export",
+  // Override the default webpack configuration
+  webpack: (config) => {
+    // Ignore node-specific modules when bundling for the browser
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias["@huggingface/transformers"] = path.resolve(
+      __dirname,
+      "node_modules/@huggingface/transformers",
+    );
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
