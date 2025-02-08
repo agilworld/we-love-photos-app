167e42bdf4c82ca338955be16bb1222120656479
dian afrial <agil.rahadi@gmail.com>
2025-02-08 23:04:12 +0700

    fix: sepeate image origin with image process component

--- src/app/components/PhotoRemoval.tsx
+++ src/app/components/PhotoRemoval.tsx
@@ -1,41 +1,51 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import { initializeModel, getModelInfo } from "@/lib/aiProcess";
 import LoaderModel from "@/components/LoaderModel";
+import { processImages } from "@/lib/aiProcess";
+import ImageProcess from "@/app/components/ImageProcess";
 
 export default function PhotoRemoval() {
   const imageRef = useRef<HTMLImageElement>(null);
   const searchParams = useSearchParams();
   const imageSrc = decodeURIComponent(searchParams.get("src") ?? "");
-  console.log(imageSrc, "img src");
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isWebGPU, setIsWebGPU] = useState<boolean>(false);
-  const [isIOS, setIsIOS] = useState<boolean>(false);
+  const [imgDimensions, setImgDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
 
   useEffect(() => {
-    (async () => {
-      try {
-        const initialized = await initializeModel();
-        if (!initialized) {
-          throw new Error("Failed to initialize background removal model");
-        }
-        const { isWebGPUSupported, isIOS: isIOSDevice } = getModelInfo();
-        setIsWebGPU(isWebGPUSupported);
-        setIsIOS(isIOSDevice);
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-    })();
+    // (async () => {
+    //   try {
+    //     const initialized = await initializeModel();
+    //     if (!initialized) {
+    //       throw new Error("Failed to initialize background removal model");
+    //     }
+    //     const { isWebGPUSupported, isIOS: isIOSDevice } = getModelInfo();
+    //     setIsWebGPU(isWebGPUSupported);
+    //     setIsIOS(isIOSDevice);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // })();
   }, []);
 
-  if (isLoading) {
-    return <LoaderModel />;
-  }
+  useEffect(() => {
+    if (imageRef.current?.offsetWidth && imageRef.current?.offsetHeight) {
+      setImgDimensions({
+        width: imageRef.current.offsetWidth,
+        height: imageRef.current.offsetHeight,
+      });
+    }
+  }, [imageRef.current]);
 
-  console.log(imageRef);
+  const handleClickProcess = useCallback(() => {}, []);
+
+  // if (isLoading) {
+  //   return <LoaderModel />;
+  // }
 
   return (
     <div className="flex items-center mx-auto mt-20 px-4 gap-10">
@@ -47,13 +57,21 @@ export default function PhotoRemoval() {
         />
       </div>
 
-      <div className="cursor-pointer w-1/2 h-64 rounded-lg border-4 border-slate-200 border-dashed">
-        <div className=""></div>
-        {/* <img
+      {imgDimensions.width > 0 && (
+        <div
+          style={{
+            width: imgDimensions.width + 4,
+            height: imgDimensions.height + 4,
+          }}
+          className="cursor-pointer w-1/2 rounded-lg border-4 border-slate-200 border-dashed"
+        >
+          <ImageProcess src={imageSrc} dimension={imgDimensions} />
+          {/* <img
           className="h-auto max-w-full rounded-lg shadow-lg"
           src={imageSrc}
         /> */}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
