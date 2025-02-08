4d636327b9c8a88a9f9107cbd9ccf374bf8c47ac
dian afrial <agil.rahadi@gmail.com>
2025-02-08 23:04:22 +0700

    fix: ehance props

--- src/components/LoaderModel.tsx
+++ src/components/LoaderModel.tsx
@@ -1,9 +1,13 @@
-export default function LoaderModel() {
+export default function LoaderModel({
+  text = "Loading background removal model",
+}: {
+  text: string;
+}) {
   return (
     <div className="min-h-screen bg-white text-gray-800 flex items-center justify-center">
       <div className="text-center">
-        <div className="inline-block animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-600 mb-4"></div>
-        <p className="text-lg">Loading background removal model</p>
+        <div className="inline-block animate-spin rounded-full h-8 w-8 border-2 border-gray-800 mb-4"></div>
+        <p className="text-lg">{text}</p>
       </div>
     </div>
   );
